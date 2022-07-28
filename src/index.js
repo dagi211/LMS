408fec127f6f113c2d081d47487104717567397f
dagi211 <dagi19deb@gmail.com>
2022-07-28 12:30:52 +0300

    updated again

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter baseUrl="/LMS">
+  <BrowserRouter baseUrl="/lib-man-sys">
     <App />
   </BrowserRouter>
 );
